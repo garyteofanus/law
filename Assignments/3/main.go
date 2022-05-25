6104d011b75fd65e98ae6c25df671e90658ca618
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-05-25 13:14:48 +0700

    Add dynamic port from env

--- Assignments/3/main.go
+++ Assignments/3/main.go
@@ -1,10 +1,17 @@
 package main
 
+import "os"
+
 func main() {
 	logger := NewLogrus("26ab3bf6-1cd3-4050-8650-aab5e08d39db-ls.logit.io", "30823")
 	server := NewServer(logger)
 	server.SetupRoutes()
-	if err := server.Start("8080"); err != nil {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := server.Start(port); err != nil {
 		logger.Fatalf("failed to start server: %v", err)
 	}
 }
